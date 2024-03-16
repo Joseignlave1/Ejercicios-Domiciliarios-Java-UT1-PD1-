8e050d1e5cdca5a4afd8459a40c8657c3e7771a9
Joseignlave1 <jose.lavecchia@correo.ucu.edu.uy>
2024-03-16 20:02:17 -0300

    Ejercicios 5 y 3

---
+++ src/Multsuma.java
@@ -0,0 +1,9 @@
+public class Multsuma {
+    public double multsuma(double a, double b , double c) {
+        return ((a*b) + c);
+    }
+    public static void main (String[] args) {
+        Multsuma multsuma1 = new Multsuma();
+        System.out.println(multsuma1.multsuma(1.0, 2.0, 3.0));
+    }
+}
